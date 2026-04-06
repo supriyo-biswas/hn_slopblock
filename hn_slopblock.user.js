7f265627735ce5ee971e98b1b0d72e4439f032a9
supriyo-biswas <supriyo.biswas.96@gmail.com>
2026-04-06 09:27:35 +0530

    Update hn_slopblock.user.js

--- hn_slopblock.user.js
+++ hn_slopblock.user.js
@@ -5,7 +5,7 @@
 // @match        https://news.ycombinator.com/*
 // @grant        none
 // @run-at       document-end
-// @version      2026.04.02.0001
+// @version      2026.04.06.0001
 // @downloadURL  https://github.com/supriyo-biswas/hn_slopblock/raw/refs/heads/master/hn_slopblock.user.js
 // @updateURL    https://github.com/supriyo-biswas/hn_slopblock/raw/refs/heads/master/hn_slopblock.user.js
 // ==/UserScript==
@@ -70,6 +70,8 @@
     'DaleBiagio',
     // https://news.ycombinator.com/item?id=47611328
     'shreyssh',
+    // https://news.ycombinator.com/item?id=47656782
+    'LeonTing1010',
   ])
 
   for (const commentElem of document.querySelectorAll('tr.comtr')) {
